4e0d06414660062843b2fc126baf54bed510c423
Thomas Berranger <tberranger@hotmail.fr>
2024-03-13 11:38:40 +0100

    Recipe model and migration created

---
+++ database/migrations/2024_03_13_102920_create_product_recipes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products_recipes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->foreignId('recipe_id')->references('id')->on('recipes');
+            $table->float('quantity');
+            $table->string('quantity_unity', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products_recipes');
+    }
+};
